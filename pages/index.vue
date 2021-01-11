77f7a61c7f1d1e5d13d8d80278f75e1d72d9e4d8
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-01-11 21:04:26 +0100

    Update Select Button

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <template>
   <main class="bg-gray-100">
     <div class="w-11/12 m-auto">
-      <div class="pt-32 w-full flex justify-end">
-        <a>Select</a>
+      <div class="pt-32 w-full flex flex-col items-end">
+        <select class="outline-none">
+            <option value="">Plus récent</option>
+            <option value="">Plus ancien</option>
+            <option value="">Ordre Alphabétique</option>
+            <option value=""></option>
+        </select>
       </div>
       <div class="w-11/12 sm:w-full m-auto justify-self-stretch pt-10 grid gap-10 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         <Card v-for="podcast in podcasts" 
@@ -20,6 +25,7 @@
 <script>
 import Card from "@/components/Card.vue"
 import { mapState } from 'vuex'
+import { reactive } from '@nuxtjs/composition-api'
 
 export default {
   components: { Card },
@@ -41,8 +47,33 @@ export default {
       this.$store.dispatch("increment")
       this.$store.dispatch("loadMore")
     }
+  },
+  setup() {
+
+    const data = reactive({
+      dropdown: false
+    })
+
+    return { data }
+
   }
 }
+      // <div class="pt-32 w-full flex flex-col items-end">
+      //   <div class="relative">
+      //     <div class="h-12 w-32 py-1 px-2 border-2 border-gray-600 text-gray-600 flex justify-center items-center cursor-pointer" @click="data.dropdown = !data.dropdown">
+      //       <p>Latest</p>
+      //       <span class="material-icons mt-1">keyboard_arrow_down</span>
+      //     </div>
+      //     <div v-if="data.dropdown"  class="absolute w-32">
+      //       <ul class="bg-white">
+      //         <li>1</li>
+      //         <li>2</li>
+      //         <li>3</li>
+      //         <li>4</li>
+      //       </ul>
+      //     </div>
+      //   </div>
+      // </div>
 </script>
 
 
