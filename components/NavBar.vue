7decd119542b2d8769d73ee1eed2a1b57c27e41c
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-01-20 16:02:22 +0100

    Update Nav Bar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,15 +3,14 @@
     <div class="w-10/12 h-full m-auto flex items-center">
       <div class="flex-1">
         <nuxt-link to="/">
-          <!-- <h2 class="text-black font-bold text-3xl"><span class="pr-1">2</span>HDP</h2> -->
           <img src="../assets/images/logo-2.png" class="w-16">
         </nuxt-link>
       </div>
       <nav>
         <ul class="flex justify-between text-black font-semibold tex-base little:text-xl">
           <li class="mx-2 first:mx-4 cursor-pointer"><nuxt-link :to="{ path: '/', hash: '#home' }">Home</nuxt-link></li>
-          <li class="mx-2 first:mx-4 cursor-pointer"><nuxt-link to="/podcasts">Podcasts</nuxt-link></li>
           <li class="mx-2 first:mx-4 cursor-pointer"><nuxt-link :to="{ path: '/', hash:'#contact' }">Contact</nuxt-link></li>
+          <li class="mx-2 first:mx-4 cursor-pointer"><nuxt-link to="/podcasts">Podcasts</nuxt-link></li>
         </ul>
       </nav>
     </div>
