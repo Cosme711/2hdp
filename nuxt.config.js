2525dffd78485a7becec14e68974b92931af30a3
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-02-28 22:41:59 +0100

    Update for Netlify deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,10 @@
-import redirectSSL from 'redirect-ssl'
-
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    htmlAttrs: {
-      lang: 'en'
-    },
     title: '2 Heures de perdues, podcast cinéma',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Chaque semaine, l’équipe de 2 heures de perdues revient avec humour et mauvaise foi sur un film pour le décrypter scène par scène. ( site non officiel )' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -35,8 +29,21 @@ export default {
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxtjs/composition-api',
-    '@nuxtjs/google-fonts'
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/pwa'
   ],
+  
+  pwa: {
+    meta: {
+      theme_color: '#150C21',
+      lang: 'fr',
+      description: 'Chaque semaine, l’équipe de 2 heures de perdues revient avec humour et mauvaise foi sur un film pour le décrypter scène par scène. ( site non officiel )',
+      ogTitle: '2 Heures de perdues, podcast cinéma',
+      ogDescription: 'Chaque semaine, l’équipe de 2 heures de perdues revient avec humour et mauvaise foi sur un film pour le décrypter scène par scène. ( site non officiel )',
+      ogSiteName: '2 Heures de perdues, podcast cinéma',
+      ogHost: 'https://www.2hdp-dev.com/'
+    }
+  },
 
   googleFonts: {
     download: true,
@@ -49,8 +56,6 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    '@nuxtjs/sitemap',
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     [
     'nuxt-fontawesome', {
@@ -74,20 +79,4 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   },
-
-  sitemap: {
-    hostname: 'https://www.2hdp-dev.com',
-    gzip: true,
-    defaults: {
-      changefreq: 'daily',
-      priority: 1,
-      lastmod: new Date()
-    }
-  },
-
-  serverMiddleware: [
-    redirectSSL.create({
-      enabled: process.env.NODE_ENV === 'production'
-     }),
-  ]
 }
