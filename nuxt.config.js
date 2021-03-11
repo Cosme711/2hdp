9be954003093fd59988d94ea2370714f1b7c78e8
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-03-11 12:04:42 +0100

    Clean code

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,14 +13,6 @@ export default {
 
   loading: { color: '#39b982' },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
-
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
@@ -72,11 +64,4 @@ export default {
       }
     ]
   ],
-
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  },
 }
